f1c08f643efc75f3774cfe8f3d6e849ba0874eb6
Iván Santibáñez <iasantibanez@indracompany.com>
2025-09-13 12:16:53 -0300

    (add): Se añade rutas API, se implementa registro y login de usuario junto a autenticación con token e implementación de localizador en español para retorno de validate en caso de error

---
+++ app/Http/Controllers/JWTAuthController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'rut' => 'required|string|max:10',
+            'nombre' => 'required|string|max:100',
+            'apellido' => 'required|string|max:100',
+            'password' => 'required|min:6'
+        ]);
+
+        $email = $request->nombre . "." . $request->apellido . "@ventasfix.cl";
+
+        $usuario = Usuario::create([
+            'rut' => $request->rut,
+            'nombre' => $request->nombre,
+            'apellido' => $request->apellido,
+            'email' => $email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = JWTAuth::fromUser($usuario);
+
+        if ($request->expectsJson()) {
+            return response()->json(
+                [
+                    'success' => true,
+                    'usuario' => $usuario,
+                    'token' => $token
+                ],
+                JsonResponse::HTTP_CREATED
+            );
+        }
+
+        $request->session()->regenerate();
+        // TODO: redirigir al inicio de la página
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (!$token = JWTAuth::attempt($credentials)) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'success' => false,
+                    'data' => null,
+                    'message' => 'Credenciales inválidas'
+                ], JsonResponse::HTTP_UNAUTHORIZED);
+            }
+            // TODO: redirigir al login con mensaje de error
+
+        }
+
+        if ($request->expectsJson()) {
+            return response()->json([
+                'success' => true,
+                'data' => $token,
+                'message' => 'Has iniciado sesión correctamente'
+            ]);
+        }
+
+        $request->session()->regenerate();
+        // TODO: redirigir al inicio de la página
+    }
+
+    public function perfil()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login')->with('status', 'Has cerrado sesión.');
+    }
+}
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -6,16 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Authenticatable
+class Usuario extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'rut',
         'nombre',
@@ -24,26 +20,26 @@ class Usuario extends Authenticatable
         'password'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+         public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
